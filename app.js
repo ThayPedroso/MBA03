31d476529f3de4193f72ebdefb7534b171663207
Thayla Pedroso <thayla.pedroso88@gmail.com>
2025-02-01 13:57:15 +0000

    database access and error handling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,4 +19,17 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  const status = error.cause || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://thaylapedroso88:jTexNeBoVSBlIDUs@mongodb.8g4dc.mongodb.net/?retryWrites=true&w=majority&appName=mongoDB",
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
